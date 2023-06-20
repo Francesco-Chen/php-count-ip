d39d1ec5ed0fc72a402d5a5fb89f4cc340b50042
Francesco-Chen <50027391+Francesco-Chen@users.noreply.github.com>
2023-06-20 21:05:06 +0200

    Update calcip.php

--- calcip.php
+++ calcip.php
@@ -4,8 +4,6 @@ $ip_num = sprintf("%u", ip2long($ip));
 $fp = fopen('ip2asn-v4.tsv', 'r');
 $tot = 0;
 $it = 0;
-$cn = 0;
-$us = 0;
 settype($tot, "integer"); 
 while (($line = fgets($fp)) !== false) {
   list($start, $end, $asn, $country) = explode("\t", $line);
@@ -18,17 +16,9 @@ while (($line = fgets($fp)) !== false) {
   if($country == "IT"){
   	$it = $it + ($end_num - $start_num) + 1;
   }
-  if($country == "CN"){
-  	$cn = $cn + ($end_num - $start_num) + 1;
-  }
-  if($country == "US"){
-    $us = $us + ($end_num - $start_num) + 1;
-  }
  
 }
 echo "Total routable ip address: $tot <br>";
 echo "ITALY routable ip address: $it <br>";
-echo "CHINA routable ip address: $cn <br>";
-echo "USA routable ip address: $us <br>";
 fclose($fp);
 ?>
